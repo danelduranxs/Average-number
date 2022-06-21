48c8fcb149d11918c0198348e0ee440b38a05fd5
danelduranxs <107886582+danelduranxs@users.noreply.github.com>
2022-06-21 15:00:20 -0500

    Add files via upload

---
+++ Average Machine Python - Daniel Durán 9B.py
@@ -0,0 +1,19 @@
+print()
+print("Average Machine with 4 numbers! - (Por Daniel Felipe Durán Martínez 9B)")
+print()
+print("(Los valores pueden ser positivos, negativos y/o decimales).")
+
+primer_numero = input("Por favor escribir un número:")
+segundo_numero = input("Por favor escribir otro número:")
+tercer_numero = input("Por favor escribir el tercer número:")
+cuarto_numero = input("Por favor escribir el último número:")
+
+
+average_machine = (float(primer_numero)+float(segundo_numero)+float(tercer_numero)+float(cuarto_numero))/4
+
+print()
+
+print("El promedio de los números",primer_numero,",",segundo_numero,",",tercer_numero,"y",cuarto_numero,"(los que usted escogió) es",average_machine)
+
+
+
